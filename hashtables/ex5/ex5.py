3c5dbb5f832a310c74db8e195d99841861248635
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-08-07 09:51:09 -0600

    Complete exercise 5, `finder`

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,14 +1,25 @@
-# Your code here
 
-
+def get_file_name(filepath):
+    last_slash = filepath.rfind("/");
+    if last_slash == -1:
+        return filepath
+    else:
+        return filepath[last_slash+1:]
 
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
-    return result
+    # This is a hash map that stores a list of file_names -> file_paths
+    name_to_paths = {}
+    for filepath in files:
+        file_name = get_file_name(filepath)
+        if file_name in name_to_paths:
+            name_to_paths[file_name].append(filepath)
+        else:
+            name_to_paths[file_name] = [filepath]
+    results = []
+    for query in queries:
+        if query in name_to_paths:
+            results.extend(name_to_paths[query])
+    return results
 
 
 if __name__ == "__main__":
