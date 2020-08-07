e973ddf877e384968fff2c915459b156bac906d2
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-08-07 09:34:06 -0600

    Complete exercise 2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,18 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    destinations = {}
+    start_ticket_dest = None
+    for ticket in tickets:
+        if ticket.source == "NONE":
+            start_ticket_dest = ticket.destination
+        else:
+            destinations[ticket.source] = ticket.destination
 
+    route = []
+    current_dest = start_ticket_dest
+    while current_dest != "NONE":
+        route.append(current_dest)
+        current_dest = destinations[current_dest]
+    route.append("NONE")
     return route
